24ee88465b3553a77647482a763697372854b41c
gagankumarg <developer.gagan@outlook.com>
2021-04-05 00:41:57 +0530

    User Controller

--- CoreAPIwithAngular/CoreAPI/Controllers/UserController.cs
+++ CoreAPIwithAngular/CoreAPI/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ModelLib;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,9 +15,11 @@ namespace CoreAPI.Controllers
     {
         // GET: api/<UserController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<User> Get()
         {
-            return new string[] { "User1", "User2" };
+            return new User[] { new User() { Name="User1",Email="User1@Email.com",Password="SHAPwd",Phone="123456789" },
+            new User() { Name="User2",Email="User2@Email.com",Password="SHAPwd",Phone="123456789" }
+            };
         }
 
         // GET api/<UserController>/5
---
+++ CoreAPIwithAngular/ModelLib/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ModelLib
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Password { get; set; }
+        public DateTime InitOn { get; }
+        public DateTime LastLoggedIn { get; }
+    }
+}
